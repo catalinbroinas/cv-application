c228d6343ee48b85f0ac70ce56066789b98b8d2a
catalinbroinas <catalinbroinas@yahoo.com>
2025-04-16 13:58:56 +0300

    feat: add Footer component
    - Add `.container-fluid` with copyright text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/App.css';
 import Header from './sections/header';
 import MainContent from './sections/MainContent/MainContent';
+import Footer from './sections/Footer';
 
 function App() {
     return (
     <>
         <Header />
         <main><MainContent /></main>
+        <Footer />
     </>
     );
 }
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,14 @@
+
+function Footer() {
+    return (
+        <footer>
+            <div className="container-fluid">
+                <p className="footer-text">
+                    © 2025 Catalin Broinas. All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
