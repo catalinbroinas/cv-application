18de6eda9a0c12417e1be963c03f6353fc679075
catalinbroinas <catalinbroinas@yahoo.com>
2025-04-17 13:31:52 +0300

    feat: add education inputs
    - Include school name, location, degree, start date, and end date fields

--- src/components/forms/EducationForm.jsx
+++ src/components/forms/EducationForm.jsx
@@ -3,6 +3,37 @@ function EducationForm() {
     return (
         <form id="education-form" className="form-section">
             <h3>Education</h3>
+
+            <div className="form-group">
+                <div className="form-outline">
+                    <input type="text" id="school-name" className="form-control" placeholder="School name" />
+                    <label htmlFor="school-name" className="form-label">School name</label>
+                </div>
+
+                <div className="form-outline">
+                    <input type="text" id="school-location" className="form-control" placeholder="Location" />
+                    <label htmlFor="school-location" className="form-label">Location</label>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <div className="form-outline">
+                    <input type="text" id="degree" className="form-control" placeholder="Degree" />
+                    <label htmlFor="degree" className="form-label">Degree</label>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <div className="form-outline">
+                    <input type="date" id="start-date" className="form-control" placeholder="Start date" />
+                    <label htmlFor="start-date" className="form-label">Start date</label>
+                </div>
+
+                <div className="form-outline">
+                    <input type="date" id="end-date" className="form-control" placeholder="End date" />
+                    <label htmlFor="end-date" className="form-label">End date</label>
+                </div>
+            </div>
         </form>
     );
 }
