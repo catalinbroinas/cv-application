dd4545160c84fd2f753f6135df707a00207dda5f
catalinbroinas <catalinbroinas@yahoo.com>
2025-04-17 13:48:59 +0300

    feat: add work experience inputs
    - Include job title, company, job description, start data, and end data fields

--- src/components/forms/ExperienceForm.jsx
+++ src/components/forms/ExperienceForm.jsx
@@ -2,7 +2,38 @@
 function ExperienceForm() {
     return (
         <form id="experience-form" className="form-section">
-            <h3>Work Experience</h3>
+            <h3 className="sub-title">Work Experience</h3>
+
+            <div className="form-group">
+                <div className="form-outline">
+                    <input type="text" id="job-title" className="form-control" placeholder="Job title" />
+                    <label htmlFor="job-title" className="form-label">Job title</label>
+                </div>
+
+                <div className="form-outline">
+                    <input type="text" id="company-name" className="form-control" placeholder="Company name" />
+                    <label htmlFor="company-name" className="form-label">Company name</label>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <div className="form-outline">
+                    <input type="date" id="experience-start-date" className="form-control" placeholder="Start date" />
+                    <label htmlFor="experience-start-date" className="form-label">Start date</label>
+                </div>
+
+                <div className="form-outline">
+                    <input type="date" id="experience-end-date" className="form-control" placeholder="End date" />
+                    <label htmlFor="experience-end-date" className="form-label">End date</label>
+                </div>
+            </div>
+
+            <dic className="form-group">
+                <div className="form-outline">
+                    <textarea id="job-description" className="form-control" placeholder="Job description"></textarea>
+                    <label htmlFor="job-description" className="form-label">Job description</label>
+                </div>
+            </dic>
         </form>
     );
 }
